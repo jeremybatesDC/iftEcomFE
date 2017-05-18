921988d3084aca19ffbf01d6b15c7b589a955222
jB <jeremy.bates@gmail.com>
2017-05-18 13:53:44 -0400

    clean

--- src/javascripts/modules/navUPDATED.js
+++ src/javascripts/modules/navUPDATED.js
@@ -178,11 +178,11 @@
 		else {
 			console.log('i was NOT open when clicked');
 			//on complete timing wasNot working for some reason, so doing manual tweens here
-			TweenMax.to(arrayOfTertiaryNavs, .3333, {
+			TweenMax.to(arrayOfTertiaryNavs, .25, {
 				className: '-=nav-list-level-3--ACTIVE',
 				ease: Power1.easeInOut
 			});
-			TweenMax.to(the3rdLevelNavOfTheItemThatHasBeenClicked, .3333, {
+			TweenMax.to(the3rdLevelNavOfTheItemThatHasBeenClicked, .25, {
 				className: '+=nav-list-level-3--ACTIVE',
 				ease: Power1.easeInOut
 			});
