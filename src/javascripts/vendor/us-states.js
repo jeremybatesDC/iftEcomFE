4e738d9016ef6ddb9eda488258ca86e31674a466
jB <jeremy.bates@gmail.com>
2017-05-23 14:28:10 -0400

    map

--- src/javascripts/vendor/us-states.js
+++ src/javascripts/vendor/us-states.js
@@ -1,18 +1,17 @@
-var map = d3.geomap.choropleth()
-    .geofile('https://d3-geomap.github.io/d3-geomap/topojson/countries/USA.json')
-    .projection(d3.geo.albersUsa)
-    .column('State')
-    //cannot seem to manipulate ID correctly
-    .unitId('fips')
-    .scale(700)
-    .zoomFactor(1)
-    .legend(false);
+// var map = d3.geomap.choropleth()
+//     .geofile('https://d3-geomap.github.io/d3-geomap/topojson/countries/USA.json')
+//     .projection(d3.geo.albersUsa)
+//     .column('StateAbbr')
+//     .unitId('fips')
+//     .scale(600)
+//     .zoomFactor(1)
+//     .legend(false);
 
-	d3.csv('https://d3-geomap.github.io/data/venture-capital.csv', 
-		function(error, data) {
-	    	d3.select('#map')
-	        	.datum(data)
-	        	.call(map.draw, map);
-		})
-	;
-;
+// 	d3.csv('https://d3-geomap.github.io/data/venture-capital.csv', 
+// 		function(error, data) {
+// 	    	d3.select('#iftMap')
+// 	        	.datum(data)
+// 	        	.call(map.draw, map);
+// 		})
+// 	;
+// ;
